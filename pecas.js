9c3b6c5c6decd948b02447a2f44c77cecf2b6164
Danna <dannabraga@gmail.com>
2022-01-19 22:27:49 -0300

    Sistema de cadastro de peças finalizado

---
+++ pecas.js
@@ -0,0 +1,24 @@
+let pesoPeca = 100
+
+if(pesoPeca > 100) {
+    console.log ("Peso maior que 100g, pode cadastrar a peça!")
+} else {
+    console.log ("Peso insuficiente, não é possível cadastrar")
+}
+
+let numeroPecas = 8
+if (numeroPecas < 10) {
+    console.log ("Ainda há espaço na caixa! Pode cadastrar a peça")
+} else {
+    console.log ("Não há espaço disponível na caixa")
+}
+
+let nomePeca = 'Disco de freio'
+ console.log ("O comprimento do nome da peça é:", nomePeca.length)
+
+ if(nomePeca.length < 3) {
+     console.log ("Nome de peça curto, não é possível cadastrar")
+ } else{ 
+    console.log ("Nome adequado, podemos cadastrar")
+ }
+ 
